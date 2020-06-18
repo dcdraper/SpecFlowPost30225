fe41b1017ac8f2b164d1e4d98d5680a633485817
David Draper <dcdraper@gmail.com>
2020-06-18 16:40:37 -0700

    Add project files.

---
+++ Specs/Example.feature
@@ -0,0 +1,18 @@
+﻿Feature: Example
+
+Scenario Outline: Add Numbers Outline
+	When I add <Number1> to <Number2>
+	Then the result is <Result>
+	Examples:
+	| Number1 | Number2 | Result |
+	| 4       | 4       | 8      |
+	| 2       | 2       | 4      |
+
+
+Scenario: Add Numbers Table
+	When I add the follwing numbers
+	| attribute | value |
+	| Number 1  | 3     |
+	| Number 2  | 3     |
+	Then the total is always correct
+	
---
+++ Steps/ExampleSteps.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+using System;
+using TechTalk.SpecFlow.Assist;
+
+namespace SpecFlowPost30225Issue_Tests.Steps
+{
+    [Binding]
+    public class ExampleSteps
+    {
+        int Result = 0;
+
+        [When(@"I add (.*) to (.*)")]
+        public void WhenIAddTo(string number1, string number2)
+        {
+            Result = Convert.ToInt32(number1) + Convert.ToInt32(number2);
+        }
+        
+        [Then(@"the result is (.*)")]
+        public void ThenTheResultIs(int result = 6)
+        {
+            Assert.True(result == Result);
+        }
+
+        [When(@"I add the follwing numbers")]
+        public void WhenIAddTheFollwingNumbers(Table table)
+        {
+            dynamic attributes = table.CreateDynamicInstance();
+
+            Result = attributes.Number1 + attributes.Number2;
+        }
+
+        [Then(@"the total is always correct")]
+        public void ThenTheTotalIsAlwaysCorrect()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
